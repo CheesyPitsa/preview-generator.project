6cd82690e4ab58bce1cbb4a50a120abe741989e4
Antonova Aleksandra <alex.andra200202@bk.ru>
2023-10-15 05:25:16 +0500

    Добавление генерации по картинкам

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import torch
 from deep_translator import GoogleTranslator
 import gradio as gr
 import os
+import sys
+import torch
+import open_clip
+from PIL import Image
+from open_clip import SimpleTokenizer
+from diffusers import DiffusionPipeline
+from transformers import CLIPFeatureExtractor, CLIPModel
 
 
 def get_by_prompt(height=800, width=800):
@@ -26,8 +33,28 @@ with gr.Blocks() as demo:
     btn.click(get_by_prompt, inputs=[], outputs=[out_img])
 
 if __name__ == "__main__":
+    sys.path.append('./images_mixing')
+
     # Подгрузка модельки dreamlike
     model_id = "dreamlike-art/dreamlike-diffusion-1.0"
     pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
     pipe = pipe.to("cuda")
+
+    # Загрузка моделей clip и coca
+    feature_extractor = CLIPFeatureExtractor.from_pretrained(
+        "laion/CLIP-ViT-B-32-laion2B-s34B-b79K"
+    )
+    clip_model = CLIPModel.from_pretrained(
+        "laion/CLIP-ViT-B-32-laion2B-s34B-b79K", torch_dtype=torch.float16
+    )
+    coca_model = open_clip.create_model('coca_ViT-L-14', pretrained='laion2B-s13B-b90k').to('cuda')
+    coca_model.dtype = torch.float16
+    coca_transform = open_clip.image_transform(
+        coca_model.visual.image_size,
+        is_train=False,
+        mean=getattr(coca_model.visual, 'image_mean', None),
+        std=getattr(coca_model.visual, 'image_std', None),
+    )
+    coca_tokenizer = SimpleTokenizer()
+
     demo.launch()
